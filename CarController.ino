2bd2addf319821135ee3a5843019d764c7aa3f6d
aarongros <14892749+aarongros@users.noreply.github.com>
2018-01-26 12:18:43 -0600

    Update CarController.ino

--- CarController.ino
+++ CarController.ino
@@ -1,21 +1,47 @@
 //Needs to send signals to arduino micro vie digital pings 3-7 to tell it which lights to turn on and off
 //needs to recieve bluetooth data from the joystick and the app
 //needs to use that data to steer and change velocity.
-int y_value;
-int x_value;
-bool c_button;
-bool z_button;
-int accel_x;
-int accel_y;
-int accel_z;
-bool headLightsOn;
-bool brakeLightsOn;
-bool leftLightsOn;
-bool rightLightsOn;
-bool hazardsOn;
+const int RIGHT_LIGHT = 3;
+const int LEFT_LIGHT = 4;
+const int BRAKE_LIGHT = 5;
+const int HAZARD_LIGHT = 6;
+const int HEAD_LIGHT = 7;
+const int RIGHT_PWM_DRIVE = 13;
+const int LEFT_PWM_DRIVE = 12;
+const int RIGHT_PWM_STEER = 10;
+const int LEFT_PWM_STEER = 9;
+#define STEER_INPUT A0;
+int y_value = 0;
+int x_value = 0;
+bool c_button = false;
+bool z_button = false;
+int accel_x = 0;
+int accel_y = 0;
+int accel_z = 0;
+int steer_pot = 0;
+bool headLightsOn = false;
+bool brakeLightsOn = false;
+bool leftLightsOn = false;
+bool rightLightsOn = false;
+bool hazardsOn = false;
+
+5 outputs for light
+2 output for steer
+2 output for drive
+1 input for steer pot
+
 
 void Setup()	{
-	
+	pinMode(RIGHT_LIGHT, OUTPUT);
+	pinMode(LEFT_LIGHT, OUTPUT);
+	pinMode(BRAKE_LIGHT, OUTPUT);
+	pinMode(HAZARD_LIGHT, OUTPUT);
+	pinMode(HEAD_LIGHT, OUTPUT);
+	pinMode(RIGHT_PWM_DRIVE, OUTPUT);
+	pinMode(LEFT_PWM_DRIVE, OUTPUT);
+	pinMode(RIGHT_PWM_STEER, OUTPUT);
+	pinMode(LEFT_PWM_STEER, OUTPUT);
+	pinMode(STEER_INPUT, INPUT);
 }
 
 void Loop()	{
@@ -24,7 +50,17 @@ void Loop()	{
 	checkTurn();
 }
 
-void checkDrive();
+void checkDrive()	{
+	analogRead();
+	analogWrite();
+}
+
+void checkTurn()	{
+	steer_pot = analogRead();
+	if(!((steet_pot >= 950 && drive > 0) || (steer_pot <= 75 && drive < 0))	{
+		analogWrite();
+	}
+}
 
 void checkLights()	{
 	checkHazards();
