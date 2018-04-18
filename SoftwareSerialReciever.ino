240d508c28b61236c846809fc941dc11b18264c9
aarongros <14892749+aarongros@users.noreply.github.com>
2018-04-18 11:33:54 -0500

    Update SoftwareSerialReciever.ino

--- SoftwareSerialReciever.ino
+++ SoftwareSerialReciever.ino
@@ -1,7 +1,11 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(10, 11);
-
+int x_axis;
+int y_axis;
+int z_button;
+int c_button;
+int x_accel;
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
@@ -14,9 +18,26 @@ void setup() {
 
 void loop() {
   if (mySerial.available()) {
-    Serial.write(mySerial.read());
+    data = mySerial.read();
+    Serial.write(data);
   }
   if (Serial.available()) {
     mySerial.write(Serial.read());
+  }
+  
+  if(data.next().equals("x")) {
+    int x_axis = data.next();
+  }
+  if(data.next().equals("y")) {
+    int y_axis = data.next();
+  }
+  if(data.next().equals("z")) {
+    int z_button = data.next();
+  }
+  if(data.next().equals("c")) {
+    int c_button = data.next();
+  }
+  if(data.next().equals("ax")) {
+    int x_accel = data.next();
   }
 }
