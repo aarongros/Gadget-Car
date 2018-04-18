8445b06be4989ade7a965c002f48be919412f1e6
aarongros <14892749+aarongros@users.noreply.github.com>
2018-04-18 11:05:21 -0500

    Update SoftwareSerialReciever.ino

--- SoftwareSerialReciever.ino
+++ SoftwareSerialReciever.ino
@@ -1,23 +1,18 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial mySerial(10, 11);
 
 void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(57600);
+  Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ;
   }
-
-
-  Serial.println("Goodnight moon!");
-
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(4800);
-  mySerial.println("Hello, world?");
+  Serial.println("Connected");
+  mySerial.begin(9600);
+  mySerial.println("Connected");
 }
 
-void loop() { // run over and over
+void loop() {
   if (mySerial.available()) {
     Serial.write(mySerial.read());
   }
